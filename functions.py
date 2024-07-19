649e96b304264be87586c1901360629e082c43a5
anindya <anindyadas1995@gmail.com>
2024-07-19 19:37:10 +0530

    update the gui and implemented the Edit button

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+FILEPATH = 'todos.txt'
+
+
 def get_todos(filepath="todos.txt"):
     """ Read a text file and return the list of
     to-do items
@@ -12,3 +15,7 @@ def write_todos(todos_arg, filepath="todos.txt"):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print('')
---
+++ gui.py
@@ -0,0 +1,44 @@
+import FreeSimpleGUI as sg
+
+import functions
+
+label = sg.Text("Type in a todo")
+input_box = sg.InputText(tooltip='Enter todo here', key='todo')
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 15])
+
+edit_button = sg.Button("Edit")
+
+window = sg.Window('My ToDo App',
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=('Helvetica', 12))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todos = values['todo'] + "\n"
+            todos.append(new_todos)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todos_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+
+            todos = functions.get_todos()
+            index = todos.index(todos_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
+
+        case sg.WIN_CLOSED:
+            break
