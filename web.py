cdb5331a365fbf67fc47c21061a4388c8e74e8c1
anindya <anindyadas1995@gmail.com>
2024-07-22 16:53:16 +0530

    Added new items features

--- web.py
+++ web.py
@@ -3,11 +3,18 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title('My ToDo App')
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='', placeholder='Add new todo...')
+st.text_input(label='', placeholder='Add new todo...',
+              on_change=add_todo, key='new_todo')
 
 
