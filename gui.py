2580d79aa32d2552bd7deec85920e3c26758b327
anindya <anindyadas1995@gmail.com>
2024-07-22 11:44:54 +0530

    implement the complete and edit button

--- gui.py
+++ gui.py
@@ -5,10 +5,14 @@ label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip='Enter todo here', key='todo')
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=(45, 10))
+                      enable_events=True, size=(30, 10))
 edit_button = sg.Button('Edit')
-
-window = sg.Window('My ToDo App', layout=[[label], [input_box, add_button], [list_box, edit_button]],
+complete_button = sg.Button("Complete")
+exit_button = sg.Button('Exit')
+window = sg.Window('My ToDo App', layout=[[label],
+                                          [input_box, add_button],
+                                          [list_box, edit_button, complete_button],
+                                          [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -34,8 +38,21 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value=' ')
+
+        case 'Exit':
+            break
+
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
         case sg.WIN_CLOSED:
             break
+
+window.close()
